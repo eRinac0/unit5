94b0fdda8308d7317fd6cd0fcbd291420f8d966d
Vladik <vladkramarenko53@gmail.com>
2023-03-25 19:34:53 +0200

    add 10 task

--- js/script10.js
+++ js/script10.js
@@ -1,10 +1,58 @@
 console.log("Завдання 10");
+function sign(num) {
+    let result1;
+    if (num>0) {
+        result1 ="Число додатне";
+            }
+        else if(num<0){
+            result1 ="Число від'ємне";
+        }
+        else if(num==0){
+            result1 ="Це 0";
+        }
+        return result1;
+}
+function simple(num) {
+    let result2;
+    if( num <= 1 )
+    return result2 = "Число не  просте";
+  for( i = 2; i * i <= num; i ++)
+  { 
+      if( num % i == 0 ) 
+      return result2 = "Число не  просте";
+  }
+  return  result2 = "Число просте";
 
-function fun(num)
+
+}
+function division(num) {
+    let result3;
+    if (num%2==0) {
+        result3 ="Число ділится без залишку на 2";
+        if (num%5==0) {
+            result3 ="Число ділится без залишку на 2,5";
+            if(num%3==0){
+                result3 ="Число ділится без залишку на 2,5,3";
+                if (num%6==0) {
+                    result3 ="Число ділится без залишку на 2,5,3,6";
+                    if(num%9==0){
+                         result3 ="Число ділится без залишку на 2,5,3,6,9";
+                    }
+                }
+            }
+        }
+    }
+    else {
+        result3 ="Число не ділится без залишку на заданні числа";
+
+    }
+    return result3;
+}
+function fun(){
 let num = Number(document.getElementById("num").value);
 
-{
-if (num>0) {
     
-}
+    document.getElementById("sign").innerHTML =sign(num);
+    document.getElementById("simple").innerHTML =simple(num);
+    document.getElementById("division").innerHTML =division(num);
 }
--- js/script9.js
+++ js/script9.js
@@ -3,7 +3,7 @@ function Add(num1,num2){
 return num1+num2;
 }
 function Sub(num1,num2){
-    return num1+num2;
+    return num1-num2;
 }
 
 function Mul(num1,num2){
